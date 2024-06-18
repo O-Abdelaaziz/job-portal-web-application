fd205b6cf90e4d1c2691339647e57b1ebc50f536
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-06-18 17:46:52 +0100

    Task 1: first commit & create job-portal-web-application

---
+++ src/main/java/com/job/portal/web/application/JobPortalWebApplication.java
@@ -0,0 +1,13 @@
+package com.job.portal.web.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/job/portal/web/application/JobPortalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.portal.web.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
